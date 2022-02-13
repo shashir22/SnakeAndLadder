363c1aad80deb9ea13cf2a70cf2aca42cea40c0e
Shashikumar <shashirgowdas67@gmail.com>
2022-02-13 23:55:53 +0530

    Added:Player exact winning position

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        public const int IsLadder = 1;
+        public const int IsSnake = 2;
+        static void Main(string[] args)
+        {
+            int PositionOfPlayer = 0;
+            while (PositionOfPlayer < 100)
+            {
+                Random random = new Random();
+                int dice = random.Next(1, 7);
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case IsLadder:
+                        Console.WriteLine("Yeah! its a ladder,please step up : ");
+                        PositionOfPlayer += dice;
+                        break;
+
+                    case IsSnake:
+                        Console.WriteLine("Opss! its a snake,you are going down : ");
+                        PositionOfPlayer -= dice;
+                        if (PositionOfPlayer < 0)
+                            PositionOfPlayer = 0;
+                        break;
+
+                    default:
+                        Console.WriteLine("You cannot move : ");
+                        break;
+                }
+                if (PositionOfPlayer > 100)
+                {
+                    PositionOfPlayer -= dice;
+                    Console.WriteLine("Cannot move ...please roll again");
+                }
+                Console.WriteLine("Position of player at initial is " + PositionOfPlayer + ",and rolled a die : " + dice);
+            }
+        }
+    }
+}
