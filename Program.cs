ad118a76465d77bd3528fb2ff4b7f37085b0e7d7
Shashikumar <shashirgowdas67@gmail.com>
2022-02-13 22:44:26 +0530

    Added:Player Starting Position

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int PositionOfPlayer = 0;
+
+            Console.WriteLine("welcome to Snake Ladder Program");
+            Console.WriteLine("Position of player at initial is " + PositionOfPlayer);
+        }
+    }
+}
