b4aa354aa34bf0529ce82efc9cf2f4201248d4db
Shashikumar <shashirgowdas67@gmail.com>
2022-02-13 23:45:36 +0530

    Added:Player check for an option

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        public const int IsLadder = 1;
+        public const int IsSnake = 2;
+        static void Main(string[] args)
+        {
+            int PositionOfPlayer = 0;
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+            int option = random.Next(0, 3);
+            switch (option)
+            {
+                case IsLadder:
+                    PositionOfPlayer += dice;
+                    break;
+
+                case IsSnake:
+                    PositionOfPlayer -= dice;
+                    if (PositionOfPlayer < 0)
+                        PositionOfPlayer = 0;
+                    break;
+
+                default: break;
+            }
+            Console.WriteLine("Position of player at initial is " + PositionOfPlayer + ",and rolled a die : " + dice);
+        }
+    }
+}
