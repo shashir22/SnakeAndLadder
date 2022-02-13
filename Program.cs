d8c50f68e65e578b1f120cf0748fe6fa036704eb
Shashikumar <shashirgowdas67@gmail.com>
2022-02-13 23:32:16 +0530

    Added:Player rolls the dice

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        public const int player = 0;
+        static void Main(string[] args)
+        {
+            int PositionOfPlayer = 0;
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+
+
+
+            Console.WriteLine("Position of player at initial is " + PositionOfPlayer + ",and rolled a die : " + dice);
+        }
+    }
+}
